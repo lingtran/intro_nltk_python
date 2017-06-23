54a289c17bc28ebe84a9e4394f97469bb6332aa7
lingtran <lxiutran@gmail.com>
2017-06-23 01:42:43 -0500

    Add Named Entity Recognition of tagged items

--- sample_sentence.py
+++ sample_sentence.py
@@ -6,12 +6,19 @@ sentence = "What is the weather in Chicago?"
 tokens = word_tokenize(sentence)
 
 print(tokens)
+# ['What', 'is', 'the', 'weather', 'in', 'Chicago', '?']
 
 stop_words = set(stopwords.words('english'))
 clean_tokens = [w for w in tokens if not w in stop_words]
 
 print(clean_tokens)
+# ['What', 'weather', 'Chicago', '?']
 
 tagged = nltk.pos_tag(clean_tokens)
 
 print(tagged)
+# [('What', 'WP'), ('weather', 'NN'), ('Chicago', 'NNP'), ('?', '.')]
+
+#ne_chunk from Numpy
+print(nltk.ne_chunk(tagged))
+# (S What/WP weather/NN (GPE Chicago/NNP) ?/.)
