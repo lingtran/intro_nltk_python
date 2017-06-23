cd4047bfea8dc0108a343712224769c4e1a59175
lingtran <lxiutran@gmail.com>
2017-06-23 01:33:35 -0500

    Clean up tokens with stop word removal to make bag of tokens concise

--- sample_sentence.py
+++ sample_sentence.py
@@ -1,7 +1,13 @@
 from nltk import word_tokenize
+from nltk.corpus import stopwords
 
 sentence = "What is the weather in Chicago?"
-
 tokens = word_tokenize(sentence)
 
 print(tokens)
+
+stop_words = set(stopwords.words('english'))
+clean_tokens = [w for w in tokens if not w in stop_words]
+clean_tokens
+
+print(clean_tokens)
